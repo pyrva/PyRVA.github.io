b79f32a34341d5d5dbb00a4d7a9d87f7b3545428
Brian Cohan <briancohan@gmail.com>
2024-06-15 22:15:42 -0400

    redirects for external sites

--- src/pyrva/app.py
+++ src/pyrva/app.py
@@ -1,12 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template
 from werkzeug.wrappers import Response
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world() -> Response:
+def index() -> str:
     return render_template("pages/index.html")
+
+
+@app.route("/meetup")
+def meetup() -> Response:
+    return redirect("https://www.meetup.com/PyRVAUserGroup/")
+
+
+@app.route("/discord")
+def discord() -> Response:
+    return redirect("https://discord.com/invite/fSGW7Jra4T")
+
+
+@app.route("/youtube")
+def youtube() -> Response:
+    return redirect("https://youtube.com/@pyrva")
 
 
 if __name__ == "__main__":

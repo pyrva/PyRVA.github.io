568a13e8d9b2ca7a5228376865b32f874ea15b7e
Brian Cohan <briancohan@gmail.com>
2024-07-02 16:21:02 -0400

    add CNAME

--- scripts/build.py
+++ scripts/build.py
@@ -9,6 +9,7 @@ from flask_frozen import Freezer
 from pyrva.app import app
 
 app.config["FREEZER_DESTINATION"] = "../../docs"
+app.config["FREEZER_DESTINATION_IGNORE"] = ["CNAME"]
 freezer = Freezer(app)
 
 if __name__ == "__main__":
